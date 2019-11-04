9c19083f70f640b73de188ed5a3e9bb9e8da7500
Rick Brown <ricky.brown.00@gmail.com>
2019-11-04 23:39:43 +0000

    updating deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [data, setData] = React.useState()
+  React.useEffect(() => {
+    axios
+      .get('/.netlify/functions/todos')
+      .then(res => {
+        setData({ data: res.data })
+      })
+      .catch(err => console.log({ err }))
+  }, [])
   return (
     <div>
       <h1>APP!!!</h1>
+      {data ? data : <p>loading..</p>}
     </div>
   )
 }
